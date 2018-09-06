1febb6a608c56dc1c945070eb28f56931fa3f663
GabrielFSilva <gabrielsilva.prog@gmail.com>
2018-09-06 15:57:52 -0300

    BlockBreaker Project (Part 1)

---
+++ Block_Breaker/Assets/Scripts/Block.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+
+    [SerializeField]
+    private AudioClip breakSound;
+
+
+    // Cached reference
+    private Level level;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.CountBreakableBlocks();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+        Destroy(gameObject);
+    }
+}
---
+++ Block_Breaker/Assets/Scripts/Level.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour {
+
+    [SerializeField]
+    private int breakableBlocks;
+
+    public void CountBreakableBlocks()
+    {
+        breakableBlocks++;
+    }
+    
+}
